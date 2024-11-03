2a05a511ce4f738e74430666e14eb6a6f0d11334
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2024-11-03 12:45:11 -0500

    examples/menu-webview.rs

--- examples/menu-webview.rs
+++ examples/menu-webview.rs
@@ -6,6 +6,9 @@ use web_view::*;
 
 use minifb::{Key, Menu, Scale, Window, WindowOptions, MENU_KEY_CTRL};
 
+use std::sync::mpsc::channel;
+use std::thread;
+
 const WIDTH: usize = 1280 / 2;
 const HEIGHT: usize = 720 / 2;
 
@@ -15,23 +18,16 @@ const COLOR_0_ID: usize = 3;
 const COLOR_1_ID: usize = 4;
 const COLOR_2_ID: usize = 5;
 const CLOSE_MENU_ID: usize = 6;
+const OPEN_WEBVIEW: usize = 7;
+const CLOSE_WEBVIEW: usize = 8;
 
 fn main() {
     let mut buffer = vec![0u32; WIDTH * HEIGHT];
 
-    let webview = web_view::builder()
-        .title("Minimal webview example")
-        .content(Content::Url("https://en.m.wikipedia.org/wiki/Main_Page"))
-        .size(800, 600)
-        .resizable(true)
-        .debug(true)
-        .user_data(())
-        .invoke_handler(|_webview, _arg| Ok(()))
-        .run()
-        .unwrap();
+    let (tx, rx) = channel::<minifb::Key>();
 
     let mut window = Window::new(
-        "Menu example - press ESC to exit",
+        "MenuWebview example - press ESC to exit",
         WIDTH,
         HEIGHT,
         WindowOptions {
@@ -45,6 +41,12 @@ fn main() {
     window.set_target_fps(60);
 
     let mut menu = Menu::new("Test").unwrap();
+
+    menu.add_item("Open WebView", OPEN_WEBVIEW).build();
+    menu.add_item("CLose WebView", CLOSE_WEBVIEW).build();
+
+    menu.add_separator();
+
     let mut sub = Menu::new("Select Color").unwrap();
 
     sub.add_item("Color 0", COLOR_0_ID)
@@ -71,13 +73,24 @@ fn main() {
 
     menu.add_sub_menu("Sub Test", &sub);
 
-    let menu_handle = window.add_menu(&menu);
+    let mut menu_handle = window.add_menu(&menu);
 
     if let Some(menus) = window.get_posix_menus() {
         println!("Menus {:?}", menus);
     }
 
     let mut color_mul = 1;
+
+    //let webview = web_view::builder()
+    //    .title("Minimal webview example")
+    //    .content(Content::Url("https://en.m.wikipedia.org/wiki/Main_Page"))
+    //    .size(800, 600)
+    //    .resizable(true)
+    //    .debug(true)
+    //    .user_data(())
+    //    .invoke_handler(|_webview, _arg| Ok(()));
+    //    .run()
+    //    .unwrap();
 
     while window.is_open() && !window.is_key_down(Key::Escape) {
         for y in 0..HEIGHT {
@@ -100,6 +113,19 @@ fn main() {
                 CLOSE_MENU_ID => {
                     println!("Removed menu id {:?}", menu_handle);
                     window.remove_menu(menu_handle);
+                }
+                OPEN_WEBVIEW => {
+                    let webview = web_view::builder()
+                        .title("Minimal webview example")
+                        .content(Content::Url("https://en.m.wikipedia.org/wiki/Main_Page"))
+                        .size(800, 600)
+                        .resizable(true)
+                        .debug(true)
+                        .user_data(())
+                        .hide_instead_of_close(true)
+                        .invoke_handler(|webview, _arg| Ok(()))
+                        .run();
+                    //.unwrap();
                 }
                 _ => (),
             }
